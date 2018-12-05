d9039ca8df208f5a98e384a7be0cd1167edaf612
markdavidmc0 <mark@dotmodus.com>
2018-12-05 21:44:31 +0200

    Added test cases.

--- test.py
+++ test.py
@@ -4,6 +4,7 @@
 import unittest
 
 from fraud_detection import FraudDetector
+import time
 
 
 class FraudDetectionTest(unittest.TestCase):
@@ -29,31 +30,121 @@ class FraudDetectionTest(unittest.TestCase):
         output = run.detector(days, expenditure)
         self.assertEqual(output, 0)
 
-    # def test_trailing_case_03(self) -> None:
-    #     """Tests 100000 trailing days with unique expenditure"""
-    #
-    #     days = 100000
-    #     with open('P2-input1.csv', newline='') as f:
-    #         expenditure = f.read()
-    #         expenditure = expenditure.split(' ')
-    #         expenditure = list(map(lambda x: int(x), expenditure))
-    #
-    #     run = FraudDetector()
-    #     output = run.detector(days, expenditure)
-    #     self.assertEqual(output, 0)
-    #
-    # def test_trailing_case_04(self) -> None:
-    #     """Tests 190000 trailing days with unique expenditure"""
-    #
-    #     days = 190000
-    #     with open('P2-input2.csv', newline='') as f:
-    #         expenditure = f.read()
-    #         expenditure = expenditure.split(' ')
-    #         expenditure = list(map(lambda x: int(x), expenditure))
-    #
-    #     run = FraudDetector()
-    #     output = run.detector(days, expenditure)
-    #     self.assertEqual(output, 0)
+    def test_days_equals_expenditure(self) -> None:
+        """Tests cases where number of trailing days equals number of expenditure days"""
+
+        days = 2
+        expenditure = [1, 4]
+
+        run = FraudDetector()
+        output = run.detector(days, expenditure)
+        self.assertEqual(output, 0)
+
+    def test_one_day(self) -> None:
+        """Tests one day entry"""
+
+        days = 1
+        expenditure = [150]
+
+        run = FraudDetector()
+        output = run.detector(days, expenditure)
+        self.assertEqual(output, 0)
+
+    def test_less_than_one(self) -> None:
+        """Tests less than one day entry"""
+
+        days = 0
+        expenditure = []
+
+        run = FraudDetector()
+        with self.assertRaises(ValueError):
+            run.detector(days, expenditure)
+
+    def test_negative_days(self) -> None:
+        """Tests negative days"""
+
+        days = -5
+        expenditure = []
+
+        run = FraudDetector()
+        with self.assertRaises(ValueError):
+            run.detector(days, expenditure)
+
+    def test_days_larger_than_expense(self) -> None:
+        """Tests days larger than expense"""
+
+        days = 5
+        expenditure = [2, 6, 7, 5]
+
+        run = FraudDetector()
+        with self.assertRaises(ValueError):
+            run.detector(days, expenditure)
+
+    def test_days_larger_than_10_power_5(self) -> None:
+        """Tests larger than 10^5 days"""
+
+        days = 10**6
+        expenditure = [2, 6, 7, 5, 7, 8, 9]
+
+        run = FraudDetector()
+        with self.assertRaises(ValueError):
+            run.detector(days, expenditure)
+
+    def test_expense_larger_than_200(self) -> None:
+        """Tests larger than 200 foe expense"""
+
+        days = 4
+        expenditure = [0, 6, 7, 5, 7, 8, 400]
+
+        run = FraudDetector()
+        with self.assertRaises(ValueError):
+            run.detector(days, expenditure)
+
+    def test_large_case_1(self) -> None:
+        """Tests 10000 trailing days with unique expenditure"""
+
+        # initial time
+        t1 = time.time()
+
+        days = 10000
+        # read in expenditure list from file
+        with open('P2-input1.csv', newline='') as f:
+            expenditure = f.read()
+            expenditure = expenditure.split(' ')
+            expenditure = list(map(lambda x: int(x), expenditure))
+
+        print('Processing lerge test case 1...')
+        run = FraudDetector()
+        output = run.detector(days, expenditure)
+        self.assertEqual(output, 406)
+
+        # user output info
+        print('Number of messages to be sent:{}'.format(output))
+        t2 = time.time()
+        print('Time taken to run test: {}s'.format(t2 - t1))
+
+    def test_large_case_2(self) -> None:
+        """Tests 80000 trailing days with unique expenditure"""
+
+        # initial time
+        t1 = time.time()
+
+        days = 80000
+        # read in expenditure list from file
+        with open('P2-input2.csv', newline='') as f:
+            expenditure = f.read()
+            expenditure = expenditure.split(' ')
+            expenditure = list(map(lambda x: int(x), expenditure))
+
+        print('Processing lerge test case 2...')
+        run = FraudDetector()
+        output = run.detector(days, expenditure)
+        self.assertEqual(output, 3)
+
+        # user output info
+        print('Number of messages to be sent: {}'.format(output))
+        t2 = time.time()
+        print('Time taken to run test: {}s'.format(t2 - t1))
 
 
 if __name__ == "__main__":
